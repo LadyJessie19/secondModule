d3779fc50e795d781c8e56364818903ea0a8b946
Jessie Moura <jessie.moura19@gmail.com>
2023-01-23 08:24:46 -0300

    primeiro componente, poh!

--- curriculo/src/components/Header.jsx
+++ curriculo/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import './Header.css'
 
 const Header = () => {
+    const randomNumber = Math.random().toFixed(1)
+    return(
     <div className="header">
-
+            <p>{randomNumber} é maior ou menor que 0.5?</p>
+        <h1>{randomNumber > 0.5 ? "maior que" : "menor que"}</h1>
     </div>
+    )
 }
 
 export default Header
